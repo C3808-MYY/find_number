5f165c3e1a36544001f9f333689c7f5ba7dd7b2c
MYY <myyaldiz@gmail.com>
2021-07-15 20:46:07 +0300

    fix bug mucizeli

--- app.js
+++ app.js
@@ -20,16 +20,21 @@ console.log(randomNumber);
 let hak = 0
 input_1.disabled = false
 buton1.disabled = false
-let bosluk = 30
+let bosluk = "36"
 let ebat = 20
 // console.log(bosluk);
 // console.log(img_margine);
+img_margine.style.marginRight = `${bosluk}%`
+// img_margine.style.marginLeft = "0%"
+  img_margine1.style.marginLeft = `${bosluk}%`
+  // img_margine1.style.marginLeft = "0%"
+  
 function butonf() {
   tut = document.querySelector("#number").value
   sayı.append(" ", tut)
   
   // console.log(hak);
-  haksayı.innerHTML = 7 - hak
+  haksayı.innerHTML = 6 - hak
   hak = hak + 1
   tut = document.querySelector("#number").value
 
@@ -39,16 +44,17 @@ function butonf() {
   const randomColor = () => {
     return `rgb(${randomcNumber()},${randomcNumber()},${randomcNumber()})`
   }
+  bosluk = bosluk - 6
   haksayı.style.backgroundColor = randomColor()
   ipucu.style.backgroundColor = randomColor()
   haksayı.style.fontSize = `${ebat}px`
   ipucu.style.fontSize = `${ebat}px`
   img_margine.style.marginRight = `${bosluk}%`
   img_margine1.style.marginLeft = `${bosluk}%`
-  bosluk = bosluk - 5
-  ebat = ebat + 9
   
-  if (tut != randomNumber && hak > 7) {
+  ebat = ebat + 9
+  console.log(hak);
+  if (tut != randomNumber && hak > 6) {
     img_margine.style.marginRight = "0px";
     img_margine1.style.marginLeft = "0px";
     document.getElementById("buz").src = "./foto/patlama.jpg"
@@ -64,16 +70,27 @@ function butonf() {
     
     
   }
-  if (tut == randomNumber) {
+  if (tut == randomNumber && hak == 7) {
+    img_margine.style.marginRight = "-20%"
+    img_margine1.style.marginLeft = "-20%"
     window.alert("tebrikleeeer")
     ipucu.innerHTML = "TEBRİKLERRR :)"
     input_1.disabled = true
     buton1.disabled = true
+  }
+  // else if (tut == randomNumber) {
+  //   window.alert("tebrikleeeer")
+  //   ipucu.innerHTML = "TEBRİKLERRR :)"
+  //   input_1.disabled = true
+  //   buton1.disabled = true
 
-  } else if (tut > randomNumber && hak < 4) {
+  // }
+   if (tut > randomNumber && hak < 7) {
     ipucu.innerHTML = "DAHA KÜÇÜK SAYI  DENEYİN"
+    // console.log("hak kucuk", hak);
 
-  } else if (tut < randomNumber && hak < 4) {
+  }  if (tut < randomNumber && hak < 7) {
     ipucu.innerHTML = "DAHA BÜYÜK SAYI DENEYİN"
+    // console.log("hak büyük",hak);
   }
 }
